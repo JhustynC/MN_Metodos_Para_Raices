3e92ce79019ead351580bf8f70f38e667bfd981b
Jhustyn <jhustyncarvajal@gmail.com>
2023-05-04 19:02:39 -0500

    Cambio Punto Fijo

--- models/punto_fijo.py
+++ models/punto_fijo.py
@@ -9,53 +9,39 @@ class PuntoFijo(MetodoHallarRaiz, IEncontrarRaices):
     def error_aproximado(self,aproxAct:int, aproxAnt:int):
         return abs((aproxAct - aproxAnt)/aproxAct) * 100
 
+
+    #TODO: Funcion para trabjar la expresion (f(x)) algebraicamente
+
     def punto_fijo(self,f,x0,tole,ite,cif):
         xn = 0
-        ea = 0
+        ea = float('inf')
         i = 0
         print("{:^3} {:^10} {:^10} ".format("i","xn","ea(%)"))
         
         while True:
+            
+            #Metodo
+            xn = f(x0)
+            self.lapoxr.append(xn)# listas raiz
+            ea = self.error_aproximado(xn,x0)
+            self.lea.append(ea)# listas eaprox
+            
+            #Tabla de valores 
+            print("{:^3.0f} {:<10.{}g} {:^2.2f}".format( i+1,xn,cif, ea))
+            x0 = xn 
+            i += 1
 
             #Criterios de finalizacion
             if ite != 0 and i>0 and i == ite:
                 print(f"Iteraciones activa {ite}")
                 print("\nSe alcanzo el numero de iteraciones")
-                self.graficarFuncion(f,[xn])  #Grafica de la funcion y puntos
+                self.graficarFuncion(f,self.lapoxr)  #Grafica de la funcion y puntos
                 return xn
             elif tole != 0 and ea < tole:
                 print("El error alcanzo el umbral, se termina de iterar")
-                self.graficarFuncion(f,[xn])  #Grafica de la funcion y puntos
+                self.graficarFuncion(f,self.lapoxr)  #Grafica de la funcion y puntos
                 return xn
             
-            xn = f(x0)
-            ea = self.error_aproximado(xn,x0)
-                
-            print("{:^3.0f} {:<10.{}g} {:^2.2f}".format( i+1,xn,cif, ea))
-            x0 = xn 
-            i += 1
-            
-        '''
-        for i in range(ite):
-            xn = f(x0)
-            ea = self.error_aproximado(xn,x0)
-            if i > 0:
-                if(ea < tole):    
-                    print("{:^3.0f} {:^11f} {:^2.2f} ".format(i+1,xn,ea)) #Se imprime para saber en que iteracion se cumple la tolerancia
-                    
-                    #Grafica de la funcion y punto
-                    self.graficarFuncion(f,[xn])
-                    #-----------------------------
-
-                    return xn
-            print("{:^3.0f} {:^11f} {:^2.2f}".format( i+1, xn, ea))
-            x0 = xn 
-        print("\nSe alcanzo el numero de iteraciones")
-        
-        #Grafica de la funcion y punto
-        self.graficarFuncion(f,[xn])
-        #-----------------------------
-        '''
 
     def hallarRaices(self, opc,tolerancia, iteraciones,cifras):
 
